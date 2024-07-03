6106cb9c6d5f30606b811931ebdd4c068d90c781
Wayne Musungu <waynemuyera17@gmail.com>
2024-07-03 12:42:30 +0300

    Update settings.py

--- drone_dispatch/settings.py
+++ drone_dispatch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure--(jj#85m)pjkq2g$fthvji6tv7f-0mbv=jnc=qg$sbf7*p+o2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
